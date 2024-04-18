2f019f3b112daec647e154f7d24a493f62d1f96b
Adrian Ramos <adrianramosrp@gmail.com>
2024-04-18 11:10:25 +0200

    Fix build.sh continuation condition

--- docs/build.sh
+++ docs/build.sh
@@ -13,15 +13,20 @@ if [ ! -x "$(command -v docker)" ]; then
     exit 1
 fi
 
-echo "Building the Docker 'latex-builder' image to compile the document... (it will take several minutes)"
-docker build . -t latex-builder
+# Build Docker image if not exists
+if docker image inspect latex-builder >/dev/null 2>&1; then
+    echo "Docker 'latex-builder' image exists locally"
+else
+    echo "Building the Docker 'latex-builder' image to compile the document... (it will take several minutes)"
+    docker build . -t latex-builder
+fi
 
 if [ $? -eq 0 ]; then
   echo "Compiling the document in the docs/build/ directory..."
   docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD/src":/data -v "$PWD/build":/build latex-builder latexmk -gg -pdf -bibtex-cond1 -quiet -outdir=/build uclmTFGesi.tex
   if [ $? -eq 0 ]; then
     echo "Document generated in ./build/uclmTFGesi.pdf"
-    # Arguments
+    # If release flag included
     if [[ $1 = "--release" ]]; then
         echo "Copying report to ./TFG_Adrian_Ramos_Report.pdf"
         cp ./build/uclmTFGesi.pdf ./TFG_Adrian_Ramos_Report.pdf
